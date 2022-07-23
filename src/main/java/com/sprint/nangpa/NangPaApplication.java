aa5a8d19afee97a6b0f4aa73e045bc31a870f3b1
parkchanyung <cas15987@gmail.com>
2022-07-23 16:51:59 +0900

    project initialize

---
+++ src/main/java/com/sprint/nangpa/NangPaApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.nangpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NangPaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NangPaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprint/nangpa/NangPaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprint.nangpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NangPaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
