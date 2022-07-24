32e8fe4aacb8fe04ec54af38883da52cd9d0050f
pcy <cas15987@gmail.com>
2022-07-24 20:28:55 +0900

    cors 적용

---
+++ src/main/java/com/sprint/nangpa/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.sprint.nangpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 웹 관련 설정
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // /이하 모든 요청에 대해 cors 적용
+                .allowedOrigins("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.DELETE.name()
+                );
+    }
+}
