d9d39a5a38e44dca575f0a07e877d6078d9cacd5
Parkchanyung <50781066+parkchanyung@users.noreply.github.com>
2022-07-26 17:17:24 +0900

    테스트 코드 주석 처리

--- src/test/java/com/sprint/nangpa/service/RecipeServiceTest.java
+++ src/test/java/com/sprint/nangpa/service/RecipeServiceTest.java
@@ -1,23 +1,23 @@
-package com.sprint.nangpa.service;
-
-
-import com.sprint.nangpa.repository.RecipeIrdntClassRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Transactional
-public class RecipeServiceTest {
-
-    @Autowired
-    RecipeIrdntClassRepository recipeIrdntClassRepository;
-    @Autowired RecipeService recipeService;
-
-}
+//package com.sprint.nangpa.service;
+//
+//
+//import com.sprint.nangpa.repository.RecipeIrdntClassRepository;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@Transactional
+//public class RecipeServiceTest {
+//
+//    @Autowired
+//    RecipeIrdntClassRepository recipeIrdntClassRepository;
+//    @Autowired RecipeService recipeService;
+//
+//}
