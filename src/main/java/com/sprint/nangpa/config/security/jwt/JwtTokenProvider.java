79f5af84cd26da421ba0db854f88b0f69a2f7993
pcy <cas15987@gmail.com>
2022-08-17 22:10:08 +0900

    bug fix

--- src/main/java/com/sprint/nangpa/config/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/sprint/nangpa/config/security/jwt/JwtTokenProvider.java
@@ -1,33 +1,26 @@
 package com.sprint.nangpa.config.security.jwt;
 
-import com.sprint.nangpa.model.User;
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.time.Duration;
-import java.util.Date;
 
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
 
-    private final JwtProperties jwtProperties;
-
-    public String makeJwtToken(User user) {
-        Date now = new Date();
-
-        return Jwts.builder()
-                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-                .setIssuer(jwtProperties.getIssuer())
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(30).toMillis()))
-                .claim("email", user.getEmail())
-                .signWith(SignatureAlgorithm.ES256, jwtProperties.getSecretKey())
-                .compact();
-    }
+//    private final JwtProperties jwtProperties;
+//
+//    public String makeJwtToken(User user) {
+//        Date now = new Date();
+//
+//        return Jwts.builder()
+//                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+//                .setIssuer(jwtProperties.getIssuer())
+//                .setIssuedAt(now)
+//                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(30).toMillis()))
+//                .claim("email", user.getEmail())
+//                .signWith(SignatureAlgorithm.ES256, jwtProperties.getSecretKey())
+//                .compact();
+//    }
 
 
 }
