2bf1cadf8d002ffccd014e7ce985a37ca24f3da2
pcy <cas15987@gmail.com>
2022-08-29 00:17:07 +0900

    cors 테스트

--- src/main/java/com/sprint/nangpa/config/WebConfig.java
+++ src/main/java/com/sprint/nangpa/config/WebConfig.java
@@ -1,20 +1,29 @@
-//package com.sprint.nangpa.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-///**
-// * 웹 관련 설정
-// */
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-//
+package com.sprint.nangpa.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 웹 관련 설정
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean(){
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CorsFilter());
+        registrationBean.addUrlPatterns("/**");
+
+        return registrationBean;
+    }
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
 //    @Override
 //    public void addCorsMappings(final CorsRegistry registry) {
 //        registry.addMapping("/**")
 //                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
 //    }
-//
-//}
+
+}
