7df375d0168bacae086ee9826f7bd5a70f928312
chujonghyun <chuwhdgus@gmail.com>
2022-08-28 14:19:13 +0900

    CORS filter 세팅 변경

--- src/main/java/com/sprint/nangpa/config/security/cors/CorsFilter.java
+++ src/main/java/com/sprint/nangpa/config/security/cors/CorsFilter.java
@@ -14,7 +14,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "https://naengdev.netlify.app/login");
+        response.setHeader("Access-Control-Allow-Origin", "https://naengdev.netlify.app");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
