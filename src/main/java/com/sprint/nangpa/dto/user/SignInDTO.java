02005997ae329fcf1cac66d3978af8faef4ec8c4
chujonghyun <chuwhdgus@gmail.com>
2022-08-28 22:36:17 +0900

    signInDTO 추가

---
+++ src/main/java/com/sprint/nangpa/dto/user/SignInDTO.java
@@ -0,0 +1,25 @@
+package com.sprint.nangpa.dto.user;
+
+public class SignInDTO {
+
+
+    /**
+     * 사용자 아이디
+     */
+    private String email;
+
+    /**
+     * 사용자 닉네임
+     */
+    private String nickname;
+
+    /**
+     * 프로필 사진
+     */
+    private String img;
+
+    /**
+     * 비밀번호
+     */
+    private String password;
+}
