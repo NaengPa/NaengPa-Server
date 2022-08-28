6ec474a15c026594492c53015c1dd7a500910866
chujonghyun <chuwhdgus@gmail.com>
2022-08-28 14:32:04 +0900

    CORS 설정 변경

--- src/main/java/com/sprint/nangpa/NangPaApplication.java
+++ src/main/java/com/sprint/nangpa/NangPaApplication.java
@@ -2,6 +2,9 @@ package com.sprint.nangpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication()
 public class NangPaApplication {
@@ -10,4 +13,14 @@ public class NangPaApplication {
         SpringApplication.run(NangPaApplication.class, args);
     }
 
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://naengdev.netlify.app");
+            }
+        };
+    }
 }
