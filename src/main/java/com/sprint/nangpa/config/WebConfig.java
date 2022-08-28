c89406306cd32510e734ba36e3aa89caebc0ab76
pcy <cas15987@gmail.com>
2022-08-28 22:36:54 +0900

    cors 테스트

--- src/main/java/com/sprint/nangpa/config/WebConfig.java
+++ src/main/java/com/sprint/nangpa/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // /이하 모든 요청에 대해 cors 적용
-                .allowedOrigins("https://naengdev.netlify.app")
+                .allowedOrigins("https://naengdev.netlify.app", "http://localhost")
                 .allowedHeaders("Authorization")
                 .allowedMethods(
                         HttpMethod.GET.name(),
