c2d3b5bcd35ec718777fc84b6e0e60e2a99fcd1a
pcy <cas15987@gmail.com>
2022-08-29 12:45:42 +0900

    *

--- src/main/java/com/sprint/nangpa/config/CorsFilter.java
+++ src/main/java/com/sprint/nangpa/config/CorsFilter.java
@@ -24,11 +24,12 @@ public class CorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Content-Type", "application/json");
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
