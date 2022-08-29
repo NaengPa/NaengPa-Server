97ed9bfe6765a8cae37b3376c5f009714b9e9866
pcy <cas15987@gmail.com>
2022-08-29 10:05:29 +0900

    cors 테스트

--- src/main/java/com/sprint/nangpa/config/WebConfig.java
+++ src/main/java/com/sprint/nangpa/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.sprint.nangpa.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -11,19 +12,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public FilterRegistrationBean getFilterRegistrationBean(){
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CorsFilter());
-        registrationBean.addUrlPatterns("/**");
-
-        return registrationBean;
-    }
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
-//    @Override
-//    public void addCorsMappings(final CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
-//    }
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
+    }
 
 }
