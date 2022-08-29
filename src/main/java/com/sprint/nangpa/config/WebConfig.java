654918f990bd23af189868b3d5c4e3d58653e083
pcy <cas15987@gmail.com>
2022-08-29 13:09:45 +0900

    *

--- src/main/java/com/sprint/nangpa/config/WebConfig.java
+++ src/main/java/com/sprint/nangpa/config/WebConfig.java
@@ -1,23 +1,23 @@
-//package com.sprint.nangpa.config;
-//
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-///**
-// * 웹 관련 설정
-// */
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-//
-//    @Override
-//    public void addCorsMappings(final CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
-//    }
-//
-//}
+package com.sprint.nangpa.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 웹 관련 설정
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
+    }
+
+}
--- src/main/java/com/sprint/nangpa/config/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/sprint/nangpa/config/security/jwt/JwtAuthenticationFilter.java
@@ -36,7 +36,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        if(!HttpMethod.OPTIONS.matches(request.getMethod())){
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        response.setHeader("Content-Type", "*");
+
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else {
             String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
             Claims claims = jwtTokenProvider.parseJwtToken(authorizationHeader);
 
