caaacc5b8ff74ac21054644bec5e182396336211
chujonghyun <chuwhdgus@gmail.com>
2022-09-01 19:50:53 +0900

    회원가입 단위 테스트 코드 작성 - 이메일, 닉네임 중복된 경우

--- src/test/java/com/sprint/nangpa/service/UserServiceTest.java
+++ src/test/java/com/sprint/nangpa/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,7 +28,7 @@ public class UserServiceTest {
     private UserMapper userMapper;
 
     /**
-     * 회원 가입 테스트
+     * 회원 가입 성공 테스트
      */
     @Test
     public void signUp_success() {
@@ -40,5 +41,48 @@ public class UserServiceTest {
         assertThat(signUpResultText).isEqualTo("회원가입에 성공했습니다.");
     }
 
+    /**
+     * 회원 가입 실패 테스트 - 이메일 중복된 경우
+     */
+    @Test
+    public void signUp_fail_duplicate_Email(){
+        //given
+        User user1 = new User("TestEmail@gmail.com", "닉네임", "비밀번호", "프로필 사진");
+        User user2 = new User("TestEmail@gmail.com", "닉네임2", "비밀번호", "프로필 사진");
+
+        //when
+        userService.signUp(user1);
+        try {
+            userService.signUp(user2); // 이메일 중복되므로 예외가 발생해야함
+        } catch (DuplicateKeyException e) {
+            System.out.println("e = " + e);
+            return ;
+        }
+
+        //then
+//        fail("이메일 중복되므로 예외가 발생해야함");
+    }
+
+    /**
+     * 회원 가입 실패 테스트 - 닉네임 중복된 경우
+     */
+    @Test
+    public void signUp_fail_duplicate_Nickname(){
+        //given
+        User user1 = new User("TestEmail@gmail.com", "닉네임", "비밀번호", "프로필 사진");
+        User user2 = new User("TestEmail2@gmail.com", "닉네임", "비밀번호", "프로필 사진");
+
+        //when
+        userService.signUp(user1);
+        try {
+            userService.signUp(user2); // 이메일 중복되므로 예외가 발생해야함
+        } catch (DuplicateKeyException e) {
+            System.out.println("e = " + e);
+            return ;
+        }
+
+        //then
+//        fail("이메일 중복되므로 예외가 발생해야함");
+    }
 
 }
