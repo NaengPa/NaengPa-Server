b2cf16ee30e2e0365addd3fcb2086b45bd12d4d2
chujonghyun <chuwhdgus@gmail.com>
2022-09-02 11:17:09 +0900

    로컬 회원가입 단위테스트 추가

--- src/test/java/com/sprint/nangpa/service/UserServiceTest.java
+++ src/test/java/com/sprint/nangpa/service/UserServiceTest.java
@@ -1,7 +1,11 @@
 package com.sprint.nangpa.service;
 
+import com.sprint.nangpa.config.security.jwt.JwtProperties;
+import com.sprint.nangpa.dto.user.SignInDto;
 import com.sprint.nangpa.mapper.UserMapper;
 import com.sprint.nangpa.model.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +30,9 @@ public class UserServiceTest {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private JwtProperties jwtProperties;
 
     /**
      * 회원 가입 성공 테스트
@@ -97,13 +104,39 @@ public class UserServiceTest {
         //when
         String signUpText1 = userService.signUp(user1);
         String signUpText2 = userService.signUp(user2);
-
-
+        
         //then
         assertThat(signUpText1).isEqualTo("회원가입에 성공했습니다.");
         assertThat(signUpText2).isEqualTo("회원가입에 성공했습니다.");
 
     }
 
+    /**
+     * 로컬 로그인 테스트
+     */
+    @Test
+    public void signInTest() throws Exception {
+        //given
+        User user = new User("TestEmail@gmail.com", "닉네임", "비밀번호", "프로필 사진");
+        userService.signUp(user); //회원가입
 
+        //when
+        SignInDto signInDto = new SignInDto();
+        signInDto.setEmail(user.getEmail());
+        signInDto.setPassword(user.getPassword());
+        String accessToken = userService.signIn(signInDto);// 로그인 해서 accessToken 받아오기
+
+        //받아온 엑세스 토큰으로 유저정보 잘 받아와지나 테스트
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(accessToken)
+                .getBody();
+
+        String email = (String) claims.get("email");
+        User resultUser = userService.getUserInfo(email);
+
+        //then
+        assertThat(resultUser.getNickname()).isEqualTo(user.getNickname());
+
+    }
 }
